2c0ac21b5df10b81a26bef2ac45299d2019c2e21
Michael Cramer <michael@bigmichi1.de>
2020-12-04 10:53:16 +0100

    publish to snapcraft

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
+from distutils.version import LooseVersion
 import os
+import sys
 
+from setuptools import __version__ as setuptools_version
 from setuptools import find_packages
 from setuptools import setup
 
@@ -9,7 +12,6 @@ version = '1.0.5'
 # specified here to avoid masking the more specific request requirements in
 # acme. See https://github.com/pypa/pip/issues/988 for more info.
 install_requires = [
-    'certbot>=1.1.0',
     'setuptools',
     'zope.interface',
     'requests',
@@ -17,6 +19,26 @@ install_requires = [
     'requests-mock',
     'parsedatetime<=2.5;python_version<"3.0"'
 ]
+
+if not os.environ.get('SNAP_BUILD'):
+    install_requires.extend([
+        'acme>=0.31.0',
+        'certbot>=1.1.0',
+    ])
+elif 'bdist_wheel' in sys.argv[1:]:
+    raise RuntimeError('Unset SNAP_BUILD when building wheels '
+                       'to include certbot dependencies.')
+if os.environ.get('SNAP_BUILD'):
+    install_requires.append('packaging')
+
+setuptools_known_environment_markers = (LooseVersion(setuptools_version) >= LooseVersion('36.2'))
+if setuptools_known_environment_markers:
+    install_requires.append('mock ; python_version < "3.3"')
+elif 'bdist_wheel' in sys.argv[1:]:
+    raise RuntimeError('Error, you are trying to build certbot wheels using an old version '
+                       'of setuptools. Version 36.2+ of setuptools is required.')
+elif sys.version_info < (3,3):
+    install_requires.append('mock')
 
 BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(BASE_PATH, "README.md")) as f:
