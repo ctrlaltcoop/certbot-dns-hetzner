30e7f19ae6307859966137f2a6ba582e575e0888
Felix Höffken <felix@ctrl.alt.coop>
2025-12-31 10:24:09 +0100

    Remove python 3.8

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     author="ctrl.alt.coop",
     author_email='kontakt@ctrl.alt.coop',
     license='Apache-2.0',
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Plugins',
@@ -40,7 +40,6 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
