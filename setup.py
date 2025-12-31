1373d0e133fb156c16a31f753fe098c59ab83a77
Felix Höffken <felix@ctrl.alt.coop>
2025-12-31 11:14:51 +0100

    Remove 3.14

--- setup.py
+++ setup.py
@@ -44,7 +44,6 @@ setup(
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
-        'Programming Language :: Python :: 3.14',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Security',
         'Topic :: System :: Installation/Setup',
