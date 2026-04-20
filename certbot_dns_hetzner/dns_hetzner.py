1a755239945921df2a2a8172701feb7c08198721
Felix Höffken <felix@ctrl.alt.coop>
2026-04-20 14:37:16 +0200

    Revert "Test"
    This rerts commit 394d792b1c7cd0e3d47010d4636bd3b4f4718108.

--- certbot_dns_hetzner/dns_hetzner.py
+++ certbot_dns_hetzner/dns_hetzner.py
@@ -74,7 +74,7 @@ class Authenticator(dns_common.DNSAuthenticator):
             )
             action.wait_until_finished()
         except APIException as apiException:
-            raise PluginError("ASDF")
+            raise PluginError(apiException)
 
     def _cleanup(self, domain, validation_name, validation):
         client = self._get_hetzner_client()
@@ -90,7 +90,7 @@ class Authenticator(dns_common.DNSAuthenticator):
             )
             action.wait_until_finished()
         except APIException as apiException:
-            raise PluginError("ASDF")
+            raise PluginError(apiException)
 
     def _get_hetzner_client(self):
         return Client(token=self.credentials.conf("api_token"))
